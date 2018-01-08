32ece1c5d34762d9948f7134e8759533950e47d1
Prakash Kavi <prakash.kavi@gmail.com>
2018-01-08 12:09:38 -0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to chat app'
+  })
 
   socket.on('createMessage', (message) => {
     console.log('Create message:', message);
-    io.emit('newMessage', {
-      from:message.from,
-      text: message.text,
+    // io.emit('newMessage', {
+    //   from:message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
       createdAt: new Date().getTime()
-    });
+    })
   });
 
   socket.on('disconnect', () => {
