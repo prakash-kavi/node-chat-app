9e88d844901309e9fc0aa8dcf1bbe5627cf506bb
Prakash Kavi <prakash.kavi@gmail.com>
2018-01-08 11:42:31 -0600

    Emit messages added

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,7 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'CPrakash',
-    text: 'Client created this message'
-  });
 });
-
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message );
